b3e840557cb6bac1f01bb37cc47dc16fc1090e02
Himanshu Rawat <himanshur651@gmail.com>
2026-07-12 13:46:25 +0530

    feat: initialize NestJS project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import {ValidationPipe} from '@nestjs/common'
+import helmet from 'helmet';
+import compression from 'compression'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet())
+  app.use(compression())
+
+  app.setGlobalPrefix('api/v1')
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true
+    })
+  )
   await app.listen(process.env.PORT ?? 3000);
+
+    console.log(
+    `Server running on http://localhost:${process.env.PORT ?? 3000}/api/v1`,
+  );
 }
 bootstrap();
