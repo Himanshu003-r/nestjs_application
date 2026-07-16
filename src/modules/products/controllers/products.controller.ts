3fe3e6d2707de93b172b262fc973908f946f32ac
Himanshu Rawat <himanshur651@gmail.com>
2026-07-16 09:55:20 +0530

    feat(products): implement product pagination

--- src/modules/products/controllers/products.controller.ts
+++ src/modules/products/controllers/products.controller.ts
@@ -6,6 +6,7 @@ import {
   Param,
   Patch,
   Post,
+  Query,
   UseGuards,
 } from '@nestjs/common';
 import { ProductsService } from '../services/products.service';
@@ -15,6 +16,7 @@ import { Roles } from 'src/modules/auth/decorator/roles.decorator';
 import { UserRole } from '@prisma/client';
 import { CreateProductDto } from '../dto/create-product.dto';
 import { UpdateProductDto } from '../dto/update-product.dto';
+import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -28,8 +30,8 @@ export class ProductsController {
   }
 
   @Get()
-  getAll() {
-    return this.productService.getAllProduct();
+  getAll(@Query() paginationQuery: PaginationQueryDto) {
+    return this.productService.getAllProduct(paginationQuery);
   }
 
   @Get(':id')
