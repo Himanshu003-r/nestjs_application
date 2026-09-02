3877b8600f66a4a2933a9a33d08cbb40427af8cd
Himanshu Rawat <himanshur651@gmail.com>
2026-09-02 12:16:55 +0530

    feat(order): complete order lifecycle and admin fulfillment

--- src/modules/order/controllers/order.controller.ts
+++ src/modules/order/controllers/order.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Param, Post, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, Patch, Post, Query, UseGuards } from '@nestjs/common';
 import { OrderService } from '../service/order.service';
 import { CurrentUser } from 'src/modules/auth/decorator/current-user.decorator';
 import type { JwtPayload } from 'src/modules/auth/interfaces/jwt-payload.interface';
 import { JwtAuthGuard } from 'src/modules/auth/guards/jwt-auth.guard';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
+import { Roles } from 'src/modules/auth/decorator/roles.decorator';
+import { UserRole } from '@prisma/client';
 
 @Controller('orders')
 export class OrderController {
@@ -25,5 +27,19 @@ export class OrderController {
   @UseGuards(JwtAuthGuard)
   getOne(@CurrentUser() user:JwtPayload, @Param('id') orderId: string){
    return this.orderService.getOrderById(user.sub, orderId)
+  }
+
+  @Patch('ship/:id')
+  @UseGuards(JwtAuthGuard)
+  @Roles(UserRole.ADMIN)
+  shipOrder(@Param('id') orderId: string){
+    return this.orderService.shipOrder(orderId)
+  }
+
+  @Patch('complete/:id')
+  @UseGuards(JwtAuthGuard)
+  @Roles(UserRole.ADMIN)
+  completeOrder(@Param('id') orderId: string){
+    return this.orderService.completeOrder(orderId)
   }
 }
